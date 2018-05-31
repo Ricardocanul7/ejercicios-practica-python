874fd4bba2eee420c5a05dfbae5c5d4b5b599e84
Ricardo Canul <ricardocanul7@gmail.com>
2018-05-30 21:35:44 -0500

    Agrega funcion para serie3 y factorial

--- suma_series.py
+++ suma_series.py
@@ -12,18 +12,40 @@ def serie1(n):
 def serie2(n):
     i = 0
     suma = 0
-    #INCOMPLETO
+    
     while i != n:
         i = float(i) + 1
         suma = suma + (1 / i)
     
     return suma
 
+# DADO UN NUMERO N, CALCULA LA SUMA DE LA SERIE 1 + 1/2! + 1/3! + ... + 1/N!
+def serie3(n):
+    i = 0
+    suma = 0
+    
+    while i != n:
+        i = float(i) + 1
+        suma = suma + (1 / float(factorial(i)))
+
+    return suma
+
+def factorial(n):
+    factorial = 1
+    i = 0
+    while i != n:
+        i = i + 1
+        factorial = factorial * i
+
+    return factorial
+
 def main():
     print("Series")
     numero = input("Ingresa un numero N: ")
 
-    print("Resultado de serie1: " + str(serie1(numero)))
+    print("\nResultado de serie1: " + str(serie1(numero)))
     print("Resultado de serie2: " + str(serie2(numero)))
+    print("Resultado de serie3: " + str(serie3(numero)))
+    print("Resultado del factorial: " + str(factorial(numero)))
 
 main()
